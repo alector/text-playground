b951aafd8078d91c60562851965d707bcad0b10a
alector <panagiotispp@gmail.com>
2021-03-26 21:52:59 +0100

    various changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require("path")
-const { getFilesRecursive } = require("./src/getfiles")
+const { getFilesRecursive, getFoldersRecursive } = require("./src/getfiles")
 
 const print = x => console.log(x)
 print("Hello")
@@ -7,5 +7,10 @@ print("Hello")
 base_path = path.dirname("")
 static_path = path.join(base_path, "static")
 print({ static_path })
-allfiles = getFilesRecursive(static_path)
-console.log(allfiles)
+
+// DO NOT REMOVE!!!
+// allfiles = getFilesRecursive(static_path)
+// console.log(allfiles)
+
+allfolders = getFoldersRecursive(static_path)
+console.log({ allfolders })
