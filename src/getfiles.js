fecf8684888a8a9324d1fddd0ee6a862bdf156ba
alector <panagiotispp@gmail.com>
2021-03-26 20:12:26 +0100

    getfiles recursive function

---
+++ src/getfiles.js
@@ -0,0 +1,34 @@
+const path = require("path")
+const fs = require("fs")
+
+const print = x => console.log(x)
+
+exports.getFilesRecursive = cur_path => {
+  // find all ENTRIES = FILES + FOLDERS in current path
+  const entries = fs.readdirSync(cur_path, { withFileTypes: true })
+  print({ cur_path })
+
+  // isolate the FILES from the ENTRIES in current path
+  const files = entries
+    .filter(file => {
+      return !file.isDirectory()
+    })
+    .map(file => {
+      filename = file.name
+      extension = filename.split(".").pop()
+      filepath = path.join(cur_path, file.name)
+      return { ...file, path: filepath, filename, ext: extension }
+    })
+
+  // isolate the FOLDERS from the ENTRIES in current path
+  const folders = entries.filter(folder => folder.isDirectory())
+
+  for (const folder of folders) {
+    console.log(folder) // { folder: Dirent { name: 'test-folder', [Symbol(type)]: 2 } }
+
+    dirPath = path.join(cur_path, folder.name)
+    files.push(...this.getFilesRecursive(dirPath))
+  }
+
+  return files
+}
