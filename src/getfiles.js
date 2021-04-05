376c9da5b7aeca58768a641395a1257196f8d6ef
alector <panagiotispp@gmail.com>
2021-04-05 12:18:02 +0200

    remove `this` from recursive function

--- src/getfiles.js
+++ src/getfiles.js
@@ -3,63 +3,70 @@ const fs = require("fs")
 
 const print = x => console.log(x)
 
-exports.getFilesRecursive = cur_path => {
-  // find all ENTRIES = FILES + FOLDERS in current path
-  const entries = fs.readdirSync(cur_path, { withFileTypes: true })
-  print({ cur_path })
+const getFilesRecursive = cur_path => {
+    // find all ENTRIES = FILES + FOLDERS in current path
+    const entries = fs.readdirSync(cur_path, { withFileTypes: true })
+    print({ cur_path })
 
-  // isolate the FILES from the ENTRIES in current path
-  const files = entries
-    .filter(file => {
-      return !file.isDirectory()
-    })
-    .map(file => {
-      filename = file.name
-      extension = filename.split(".").pop()
-      filepath = path.join(cur_path, file.name)
-      return { ...file, path: filepath, filename, ext: extension }
-    })
+    // isolate the FILES from the ENTRIES in current path
+    const files = entries
+        .filter(file => {
+            return !file.isDirectory()
+        })
+        .map(file => {
+            filename = file.name
+            extension = filename.split(".").pop()
+            filepath = path.join(cur_path, file.name)
+            return { ...file, path: filepath, filename, ext: extension }
+        })
 
-  // isolate the FOLDERS from the ENTRIES in current path
-  const folders = entries.filter(folder => folder.isDirectory())
+    // isolate the FOLDERS from the ENTRIES in current path
+    const folders = entries.filter(folder => folder.isDirectory())
 
-  for (const folder of folders) {
-    console.log(folder) // { folder: Dirent { name: 'test-folder', [Symbol(type)]: 2 } }
+    for (const folder of folders) {
+        console.log(folder) // { folder: Dirent { name: 'test-folder', [Symbol(type)]: 2 } }
 
-    dirPath = path.join(cur_path, folder.name)
-    files.push(...this.getFilesRecursive(dirPath))
-  }
+        dirPath = path.join(cur_path, folder.name)
+        files.push(...getFilesRecursive(dirPath))
+    }
 
-  return files
+    return files
 }
 
-exports.getFoldersRecursive = cur_path => {
-  // find all ENTRIES = FILES + FOLDERS in current path
-  // print({ cur_path })
+const getFoldersRecursive = cur_path => {
+    // find all ENTRIES = FILES + FOLDERS in current path
+    // print({ cur_path })
 
-  const entries = fs.readdirSync(cur_path, { withFileTypes: true })
-  // print({ entries })
+    const entries = fs.readdirSync(cur_path, { withFileTypes: true })
+    // print({ entries })
 
-  // isolate the FOLDERS from the ENTRIES in current path
+    // isolate the FOLDERS from the ENTRIES in current path
 
-  const folders = entries
-    .filter(folder => folder.isDirectory())
-    .map(folder => {
-      parentpath = cur_path
-      foldername = folder.name
-      folderpath = path.join(parentpath, foldername)
-      return {
-        parentpath,
-        foldername,
-        folderpath
-      }
-    })
+    const folders = entries
+        .filter(folder => folder.isDirectory())
+        .map(folder => {
+            parentpath = cur_path
+            foldername = folder.name
+            folderpath = path.join(parentpath, foldername)
+            return {
+                parentpath,
+                foldername,
+                folderpath
+            }
+        })
 
-  // print({ folders })
+    // print({ folders })
 
-  for (const folder of folders) {
-    folders.push(...this.getFoldersRecursive(folder.folderpath))
-  }
+    for (const folder of folders) {
+        folders.push(...getFoldersRecursive(folder.folderpath))
+    }
 
-  return folders
+    return folders
 }
+
+// // if (require.main === module) {
+// 
+//     break
+// }
+
+module.exports = { getFoldersRecursive, getFilesRecursive }
